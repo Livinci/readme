cc091cd73d50a63c98e1002e4e6bd37872c33f19
Eric Liu <a0011315@gmail.com>
2017-05-20 15:30:59 +0800

    repo init and upload

---
+++ index.android.js
@@ -0,0 +1,206 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Alert,
+} from 'react-native';
+
+import {
+    StackNavigator
+} from 'react-navigation';
+
+import * as firebase from "firebase";
+
+const onButtonPress= ()=>{Alert.alert('Button has been pressed!')}
+const onButtonPress1= ()=>{Alert.alert('Register function is not ready yet!')}
+
+export default class HomeScreen extends Component {
+
+  static navigationOptions= {
+      title: 'Welcome',
+      header: null
+  };
+
+
+  constructor(props) {
+      super(props);
+      this.state = { 
+        account: 'account@gmail.com',
+        password: 'password'
+      };
+      firebase.initializeApp({
+      apiKey: "AIzaSyAHrUnwILI37oGBwQqYf4CE7unj3XOKufY",
+      authDomain: "rn-demo.firebaseapp.com",
+      databaseURL: "https://rn-demo.firebaseio.com",
+      storageBucket: "rn-demo.appspot.com"
+      })
+  };
+  onSignupPress(){
+    this.signup(this.state.account, this.state.password);
+    };
+  
+  async signup(email, pass) {
+
+    try {
+        await firebase.auth()
+            .createUserWithEmailAndPassword(email, pass);
+
+        console.log("Account created");
+
+        // Navigate to the Home page, the user is auto logged in
+
+    }
+     catch (error) {
+        console.log(error.toString())
+    }
+  };
+ 
+  render() {
+
+    const {navigate} = this.props.navigation;
+
+    return (
+    <View style={{flex:1, flexDirection: 'column'}}>
+      <View style={{flex: 2, flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'skyblue',}}>
+       
+        <Text style={{fontSize: 50,color: 'black'}}>
+         Sign in System
+        </Text>
+      
+        <Text style={{fontSize: 40,color: 'black'}}>
+         Welcome
+        </Text>
+        </View>
+      
+      <View style={{flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: 'skyblue',}}>
+        <View style = {{flex:1, flexDirection: 'row',
+        justifyContent: 'center', alignItems: 'center'}}>
+        
+        <Text style = {{fontSize: 25,color: 'black'}}>
+         Account: 
+        </Text> 
+        <TextInput
+          style = {{ height: 40, width:238, borderColor: 'gray', borderWidth: 0}}
+         
+          underlineColorAndroid = 'black'
+          onChangeText = {(text) => this.setState({account:text})}
+          value = {this.state.account}
+        />
+        </View>
+        <View style= {{flex:1, flexDirection: 'row',
+        justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{ fontSize: 25,color: 'black'}}>
+         Password: 
+        </Text>
+         <TextInput
+          style = {{ height: 40, width:238, borderColor: 'gray', borderWidth: 0}}
+        
+          keyboardType = 'numeric'
+          underlineColorAndroid = 'black'
+          onChangeText = {(text) => this.setState({password:text})}
+          value = {this.state.password}
+        />
+        </View>
+       
+      </View>
+        <View style={{flex: 1, flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        backgroundColor: 'skyblue',}}>
+        <Button 
+        onPress = {()=> navigate('Chat')}
+        title= 'Sign In!'/>
+        
+        <Button 
+        onPress = {()=> this.onSignupPress()}
+        title= 'Sign Up!'/>
+        
+       </View>
+    </View>
+     
+      
+    
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+   
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'skyblue',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+class ChatScreen extends Component {
+         static navigationOptions = {
+            title: 'Chat with Me',
+            
+         };
+     render() {
+        return (
+         <View style={{flex: 1, flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        backgroundColor: 'skyblue',}}>
+          <View style= {{flex:1, flexDirection: 'column', 
+          justifyContent: 'space-around',
+          alignItems: 'center',
+          backgroundColor: 'skyblue',
+          }}>
+           <Text style= {{fontSize: 50}}> Chat with Me</Text>
+          </View>
+          <View style= {{
+            flex:1, 
+            flexDirection: 'row', 
+            justifyContent:'space-around',
+            alignItems: 'center'
+            }}>
+            <Text style= {{fontSize: 25, alignItems:'center',}}>
+              My First HyperLink!!
+            </Text> 
+          </View>
+         </View>
+
+     );
+   }
+ }
+
+const SimpleApp= StackNavigator({
+    Home:{screen:HomeScreen},
+    Chat:{screen:ChatScreen},
+});
+
+AppRegistry.registerComponent('goodproject', () => SimpleApp);
+
+
+
---
+++ src/sample.js
@@ -0,0 +1,35 @@
+class originClass {
+
+    constructor (){
+
+
+    }
+
+    functionA() {
+
+
+    }
+
+
+    functionB() {
+
+
+
+    }
+}
+
+class addnumberClass {
+
+    constructor(){
+        this.classNumber = 50;
+
+
+    }
+
+    addnumber(n1, n2){
+        return n1+n2-this.classNumber;
+
+    }
+
+}
+
